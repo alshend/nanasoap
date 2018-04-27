453c5b6b8320664af655a0724bcd93e42de2319b
alshend <alshend@otpusk.com>
2018-04-27 17:50:58 +0300

    header.php and functions.php was refactored.

--- wp-content/themes/nanasoap/header.php
+++ wp-content/themes/nanasoap/header.php
@@ -16,37 +16,20 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
-
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<?php
-    $x = 'abc';
-    $x = apply_filters( 'misha_filter', $x );
-    echo '$x = ' . $x
-?>
 <div id="main-container">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'nanasoap' ); ?></a>
-
 	<header id="main-header" class="header">
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
 			$nanasoap_description = get_bloginfo( 'description', 'display' );
 			if ( $nanasoap_description || is_customize_preview() ) :
 				?>
 				<p class="site-description"><?php echo $nanasoap_description; ?></p>
 			<?php endif; ?>
-		</div><!-- .site-branding -->
-	</header><!-- #masthead -->
+		</div>
+	</header>
 	<div id="content" class="site-content">
